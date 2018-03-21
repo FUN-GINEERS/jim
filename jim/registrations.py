1f91cf5994c1a4df3e137175c8f8bc82bcd0663c
Mark Weiman <mark.weiman@markzz.com>
2018-03-21 14:29:22 -0400

    Moved command registrations
    Signed-off-by: Mark Weiman <mark.weiman@markzz.com>

---
+++ jim/registrations.py
@@ -0,0 +1,16 @@
+from jim import cmd_funcs, util
+
+
+def register_cmds():
+    std_perms = ['Administrator', 'Moderator', 'Fungineer+']
+    util.register_cmd("8ball", "Ask the magic 8 ball.", None, 2, cmd_funcs.eight_ball)
+    util.register_cmd("addcom", "Adds a custom command.", std_perms, 3, cmd_funcs.addcom)
+    util.register_cmd("about", "Gets general bot information.", None, 1, cmd_funcs.about)
+    util.register_cmd("archive", "Archives a channel.", std_perms, 2, cmd_funcs.archive)
+    util.register_cmd("delcom", "Deletes a custom command.", std_perms, 2, cmd_funcs.delcom)
+    util.register_cmd("mcinfo", "Get information on the Minecraft server", None, 1, cmd_funcs.mcinfo)
+    util.register_cmd("murder", "Murders a person.", None, 2, cmd_funcs.murder)
+    util.register_cmd("namechange", "Change my nickname on that server.", std_perms, 2, cmd_funcs.namechange)
+    util.register_cmd("roll", "Roll a die.", None, 2, cmd_funcs.roll)
+    util.register_cmd("ping", "Ping the bot.", None, 1, cmd_funcs.ping)
+    util.register_cmd("willie", "Ask Willie a question.", None, 2, cmd_funcs.willie)
--- jim/scripts/bot.py
+++ jim/scripts/bot.py
@@ -3,7 +3,7 @@ import time
 import datetime
 
 import jim.config as config
-from jim import cmd_funcs, util
+from jim import registrations, util
 
 client = discord.Client()
 stream = {}
@@ -12,19 +12,7 @@ stream = {}
 @client.event
 async def on_ready():
     print('Logged in as %s (%s)' % (client.user.name, client.user.id,))
-
-    std_perms = ['Administrator', 'Moderator', 'Fungineer+']
-    util.register_cmd("8ball", "Ask the magic 8 ball.", None, 2, cmd_funcs.eight_ball)
-    util.register_cmd("addcom", "Adds a custom command.", std_perms, 3, cmd_funcs.addcom)
-    util.register_cmd("about", "Gets general bot information.", None, 1, cmd_funcs.about)
-    util.register_cmd("archive", "Archives a channel.", std_perms, 2, cmd_funcs.archive)
-    util.register_cmd("delcom", "Deletes a custom command.", std_perms, 2, cmd_funcs.delcom)
-    util.register_cmd("mcinfo", "Get information on the Minecraft server", None, 1, cmd_funcs.mcinfo)
-    util.register_cmd("murder", "Murders a person.", None, 2, cmd_funcs.murder)
-    util.register_cmd("namechange", "Change my nickname on that server.", std_perms, 2, cmd_funcs.namechange)
-    util.register_cmd("roll", "Roll a die.", None, 2, cmd_funcs.roll)
-    util.register_cmd("ping", "Ping the bot.", None, 1, cmd_funcs.ping)
-    util.register_cmd("willie", "Ask Willie a question.", None, 2, cmd_funcs.willie)
+    registrations.register_cmds()
 
 
 @client.event
