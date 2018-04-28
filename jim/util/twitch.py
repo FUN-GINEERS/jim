1bbe46acaf6504f9197f925d3b864cff9ce05553
Mark Weiman <mark.weiman@markzz.com>
2018-04-27 22:22:06 -0400

    Fix silly mistake in twitch notify
    Signed-off-by: Mark Weiman <mark.weiman@markzz.com>

--- jim/util/twitch.py
+++ jim/util/twitch.py
@@ -5,7 +5,7 @@ stream = {}
 
 # this only will run on the FUN-GINEERS discord. If this needs to be changed, more needs to be done
 async def notify(client, member):
-    if not member.server.id == '234149610652696577':
+    if member.server.id == '234149610652696577':
         if member.game is not None and member.game.type == 1:
             if member.id not in stream or datetime.datetime.now() > stream[member.id]:
                 tc = client.get_channel('348820905688039444')
